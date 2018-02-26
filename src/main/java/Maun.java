ddc49af924459f4bc65b22f5f7c939ae95fc8edb
Dima <kachanovskiyfanat.99@gmail.com>
2018-02-26 19:55:45 +0300

    я вас вразумил #.!.#

--- src/main/java/Maun.java
+++ src/main/java/Maun.java
@@ -1,5 +1,14 @@
 package java;
 
-public class Maun {
-    // fdfdkjds
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("вы");
+        System.out.println("меня");
+        System.out.println("*****");
+        System.out.println("******");
+        System.out.println("***********");
+        System.out.println("***************");
+        System.out.println("#.!.#");
+    }
 }
+
